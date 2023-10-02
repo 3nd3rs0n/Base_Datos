3d63afa6ad9f509a13b589059eb9befacbecbfcf
Ender <torresender96@gmail.com>
2023-10-02 20:52:54 -0300

    agregando componentes

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Listado from "./components/Listado/Listado"
+import Alert from "./components/Alert/Alert"
+
+
+const App = () => {
+  return (
+    <>
+    <h1>hola mundo</h1>
+    <Listado/>
+    <Alert/>
+
+
+    </>
+  )
+}
+
+export default App
